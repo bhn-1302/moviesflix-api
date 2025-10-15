9a0964ebb21df48734d14c9802511663d240aa52
Brenno Henrique do Nascimento <nascimenttobrenno1302@gmail.com>
2025-10-14 21:20:34 -0300

    configurações iniciais do prima

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ const port = 3000; // porta
 const app = express();
 
 app.get("/movies", (req, res) => {
-    res.send("Listagem de Filmes");
+  res.send("Listagem de Filmes");
 });
 
 app.listen(port, () => {
-    console.log(`Servidor em execução na porta ${port}`);
+  console.log(`Servidor em execução na porta ${port}`);
 });
