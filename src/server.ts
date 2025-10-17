dd1ea19802b1eafde9a1059cff488e4a395daaef
Brenno Henrique do Nascimento <nascimenttobrenno1302@gmail.com>
2025-10-17 19:14:56 -0300

    adicionado filtro de filmes por gênero

--- src/server.ts
+++ src/server.ts
@@ -56,54 +56,85 @@ app.put("/movies/:id", async (req, res) => {
   const id = Number(req.params.id);
 
   try {
-  const movie = await prisma.movie.findUnique({
-    where: {
-      id
+    const movie = await prisma.movie.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!movie) {
+      return res.status(404).send({ message: "Filme não encontrado" });
     }
-  })
 
-  if(!movie) {
-    return res.status(404).send({message: "Filme não encontrado"})
+    const data = { ...req.body };
+    data.release_date = data.release_date
+      ? new Date(data.release_date)
+      : undefined;
+
+    //pegar os dados do filme que será atualizado e atualizar ele com um método do prisma
+    await prisma.movie.update({
+      where: {
+        id,
+      },
+      data: data,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Falha ao atualizar o registro do filme" });
   }
-
-  const data = { ...req.body };
-  data.release_date = data.release_date
-    ? new Date(data.release_date)
-    : undefined;
-
-  //pegar os dados do filme que será atualizado e atualizar ele com um método do prisma
-  await prisma.movie.update({
-    where: {
-      id,
-    },
-    data: data,
-  });
-}catch(error) {
-  return res.status(500).send({message: "Falha ao atualizar o registro do filme"})
-}  
   //retornar o status correto informando que o filme foi atualizado
   res.status(200).send();
-})
+});
 
-app.delete("/movies/:id", async(req, res) => {
-  const id = Number(req.params.id) 
+app.delete("/movies/:id", async (req, res) => {
+  const id = Number(req.params.id);
 
-  try{
-  const movie = await prisma.movie.findUnique({where: {id}})
+  try {
+    const movie = await prisma.movie.findUnique({ where: { id } });
 
-  if(!movie){
-    return res.status(404).send({message: "Filme não encontrado"})
-  }
-
-  await prisma.movie.delete({
-    where: {
-      id
+    if (!movie) {
+      return res.status(404).send({ message: "Filme não encontrado" });
     }
-  })} catch(error) {
-    return res.status(500).send({message: "Não foi possível remover o filme"})
+
+    await prisma.movie.delete({
+      where: {
+        id,
+      },
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Não foi possível remover o filme" });
   }
-  res.status(200).send()
-})
+  res.status(200).send();
+});
+
+app.get("/movies/:genreName", async (req, res) => {
+  const genreName = req.params.genreName;
+
+  try {
+    const moviesFilteredByGenreName = await prisma.movie.findMany({
+      include: {
+        genres: true,
+        languages: true,
+      },
+      where: {
+        genres: {
+          name: {
+            equals: genreName,
+            mode: "insensitive",
+          },
+        },
+      },
+    });
+    res.status(200).send(moviesFilteredByGenreName);
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Erro ao filtrar filmes por gênero" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
