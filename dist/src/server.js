bd32f677252c22225991758cda02c2dc38c11113
Brenno Henrique do Nascimento <nascimenttobrenno1302@gmail.com>
2025-10-18 18:48:12 -0300

    build do projeto com npm run build + npm start

---
+++ dist/src/server.js
@@ -0,0 +1,130 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from '../swagger.json' with { type: 'json' };
+const port = 3000;
+const app = express();
+const prisma = new PrismaClient();
+app.get("/movies", async (_, res) => {
+    const movies = await prisma.movie.findMany({
+        orderBy: {
+            title: "asc",
+        },
+        include: {
+            genres: true,
+            languages: true,
+        },
+    });
+    res.json(movies);
+});
+app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.post("/movies", async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+    try {
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: {
+                title: { equals: title, mode: "insensitive" },
+            },
+        });
+        if (movieWithSameTitle) {
+            return res
+                .status(409)
+                .send({ message: "Já existe um filme cadastrado com este título" });
+        }
+        await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date),
+            },
+        });
+    }
+    catch (error) {
+        return res.status(500).send({ message: "Falha ao cadastrar um filme" });
+    }
+    res.status(201).send();
+});
+app.put("/movies/:id", async (req, res) => {
+    //pegar o id do registro que vai ser atualizado
+    const id = Number(req.params.id);
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: {
+                id,
+            },
+        });
+        if (!movie) {
+            return res.status(404).send({ message: "Filme não encontrado" });
+        }
+        const data = { ...req.body };
+        data.release_date = data.release_date
+            ? new Date(data.release_date)
+            : undefined;
+        //pegar os dados do filme que será atualizado e atualizar ele com um método do prisma
+        await prisma.movie.update({
+            where: {
+                id,
+            },
+            data: data,
+        });
+    }
+    catch (error) {
+        return res
+            .status(500)
+            .send({ message: "Falha ao atualizar o registro do filme" });
+    }
+    //retornar o status correto informando que o filme foi atualizado
+    res.status(200).send();
+});
+app.delete("/movies/:id", async (req, res) => {
+    const id = Number(req.params.id);
+    try {
+        const movie = await prisma.movie.findUnique({ where: { id } });
+        if (!movie) {
+            return res.status(404).send({ message: "Filme não encontrado" });
+        }
+        await prisma.movie.delete({
+            where: {
+                id,
+            },
+        });
+    }
+    catch (error) {
+        return res
+            .status(500)
+            .send({ message: "Não foi possível remover o filme" });
+    }
+    res.status(200).send();
+});
+app.get("/movies/:genreName", async (req, res) => {
+    const genreName = req.params.genreName;
+    try {
+        const moviesFilteredByGenreName = await prisma.movie.findMany({
+            include: {
+                genres: true,
+                languages: true,
+            },
+            where: {
+                genres: {
+                    name: {
+                        equals: genreName,
+                        mode: "insensitive",
+                    },
+                },
+            },
+        });
+        res.status(200).send(moviesFilteredByGenreName);
+    }
+    catch (error) {
+        return res
+            .status(500)
+            .send({ message: "Erro ao filtrar filmes por gênero" });
+    }
+});
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
+//# sourceMappingURL=server.js.map
